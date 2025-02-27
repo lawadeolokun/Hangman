eb4c4e0e0bc18b6523ad3aa48f54142c15d29dbf
Lawrence <B00153847@mytudublin.ie>
2025-02-27 19:21:13 +0000

    Functions added

--- hangman.py
+++ hangman.py
@@ -1,6 +1,20 @@
 import random
 
-# random words to choose from
+# wordbank of random words to choose from
 # (for now it is hardcoded but should be moved to file or API)
 
 word_list = ('love', 'happy', 'football', 'basketball', 'sports', 'monumental', 'astronomical', 'onomatopoeia', 'inspirational', 'Ireland', 'Lawrence', 'Adeolokun')
+
+# this function chooses a random word from the wordbank
+def chooseWord():
+    return random.choice(word_list)
+
+# this functions displays the correct words guessed so far
+def displayWord(word, guessedLetters):
+    display = ''
+    for letter in word:
+        if letter in guessedLetters:
+            display in letter + ''
+        else:
+            display += '_'
+        return display.strip()
