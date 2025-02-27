f8ef1f4c02b036aae0902e268db03b826e97e908
Lawrence <B00153847@mytudublin.ie>
2025-02-27 18:45:43 +0000

    Hardcoded wordbank added

---
+++ hangman.py
@@ -0,0 +1,6 @@
+import random
+
+# random words to choose from
+# (for now it is hardcoded but should be moved to file or API)
+
+word_list = ('love', 'happy', 'football', 'basketball', 'sports', 'monumental', 'astronomical', 'onomatopoeia', 'inspirational', 'Ireland', 'Lawrence', 'Adeolokun')
